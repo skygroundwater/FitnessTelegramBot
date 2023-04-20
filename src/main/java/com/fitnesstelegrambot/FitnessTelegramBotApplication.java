48c6e12cc45bfd399e3f3915b569e1f2113d6915
Nitro <o.metelev2020@yandex.ru>
2023-04-20 08:39:25 +0300

    Initial commit

---
+++ src/main/java/com/fitnesstelegrambot/FitnessTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesstelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitnesstelegrambot/FitnessTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesstelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
